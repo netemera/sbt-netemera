19e391320152a881db211011fe33cf473d0c7e91
Michał Węgrzyn <mwegrz@netemera.com>
2020-03-01 22:06:18 +0100

    Fixes

--- src/main/scala/com/netemera/sbtnetemera/NetemeraHttpServerAppPlugin.scala
+++ src/main/scala/com/netemera/sbtnetemera/NetemeraHttpServerAppPlugin.scala
@@ -20,7 +20,19 @@ object NetemeraHttpServerAppPlugin extends MwegrzHttpServerAppPlugin {
         url = url("https://www.netemera.com")
       )
     ),
-    resolvers += "MyMavenRepo read" at "https://mymavenrepo.com/repo/zghBEmhHgl0OkEqxC1RO",
+    credentials ++= sys.env
+      .get("MY_MAVEN_REPO_PASSWORD")
+      .fold(Seq.empty[Credentials])(
+        password =>
+          Seq(
+            Credentials("mymavenrepo.com.read", "mymavenrepo.com", "myMavenRepo", password),
+            Credentials("mymavenrepo.com.write", "mymavenrepo.com", "myMavenRepo", password)
+          )
+      ),
+    resolvers ++= Seq(
+      Resolver.sonatypeRepo("snapshots"),
+      "MyMavenRepo read" at "https://mymavenrepo.com/repo/zghBEmhHgl0OkEqxC1RO"
+    ),
     maintainer := "Netemera <team@netemera.com>",
     dockerRepository := Some("registry.gitlab.com"),
     dockerUsername := Some("netemera")
--- src/main/scala/com/netemera/sbtnetemera/NetemeraLibraryPlugin.scala
+++ src/main/scala/com/netemera/sbtnetemera/NetemeraLibraryPlugin.scala
@@ -18,7 +18,19 @@ object NetemeraLibraryPlugin extends MwegrzLibraryPlugin {
         url = url("https://www.netemera.com")
       )
     ),
-    resolvers += "MyMavenRepo read" at "https://mymavenrepo.com/repo/zghBEmhHgl0OkEqxC1RO",
+    credentials ++= sys.env
+      .get("MY_MAVEN_REPO_PASSWORD")
+      .fold(Seq.empty[Credentials])(
+        password =>
+          Seq(
+            Credentials("mymavenrepo.com.read", "mymavenrepo.com", "myMavenRepo", password),
+            Credentials("mymavenrepo.com.write", "mymavenrepo.com", "myMavenRepo", password)
+          )
+      ),
+    resolvers ++= Seq(
+      Resolver.sonatypeRepo("snapshots"),
+      "MyMavenRepo read" at "https://mymavenrepo.com/repo/zghBEmhHgl0OkEqxC1RO"
+    ),
     publishTo := Some("MyMavenRepo write" at "https://mymavenrepo.com/repo/dWLBAjbgqRQN6dpgLsm5")
   )
 }
