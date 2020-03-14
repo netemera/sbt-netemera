937b30c09fd834b90f01bcb4e1f49c934ae32c3f
Michał Węgrzyn <mwegrz@netemera.com>
2020-03-14 19:54:37 +0100

    Bump deps

--- src/main/scala/com/netemera/sbtnetemera/NetemeraHttpServerAppPlugin.scala
+++ src/main/scala/com/netemera/sbtnetemera/NetemeraHttpServerAppPlugin.scala
@@ -22,12 +22,11 @@ object NetemeraHttpServerAppPlugin extends MwegrzHttpServerAppPlugin {
     ),
     credentials ++= sys.env
       .get("MY_MAVEN_REPO_PASSWORD")
-      .fold(Seq.empty[Credentials])(
-        password =>
-          Seq(
-            Credentials("mymavenrepo.com.read", "mymavenrepo.com", "myMavenRepo", password),
-            Credentials("mymavenrepo.com.write", "mymavenrepo.com", "myMavenRepo", password)
-          )
+      .fold(Seq.empty[Credentials])(password =>
+        Seq(
+          Credentials("mymavenrepo.com.read", "mymavenrepo.com", "myMavenRepo", password),
+          Credentials("mymavenrepo.com.write", "mymavenrepo.com", "myMavenRepo", password)
+        )
       ),
     resolvers ++= Seq(
       Resolver.sonatypeRepo("snapshots"),
--- src/main/scala/com/netemera/sbtnetemera/NetemeraLibraryPlugin.scala
+++ src/main/scala/com/netemera/sbtnetemera/NetemeraLibraryPlugin.scala
@@ -20,12 +20,11 @@ object NetemeraLibraryPlugin extends MwegrzLibraryPlugin {
     ),
     credentials ++= sys.env
       .get("MY_MAVEN_REPO_PASSWORD")
-      .fold(Seq.empty[Credentials])(
-        password =>
-          Seq(
-            Credentials("mymavenrepo.com.read", "mymavenrepo.com", "myMavenRepo", password),
-            Credentials("mymavenrepo.com.write", "mymavenrepo.com", "myMavenRepo", password)
-          )
+      .fold(Seq.empty[Credentials])(password =>
+        Seq(
+          Credentials("mymavenrepo.com.read", "mymavenrepo.com", "myMavenRepo", password),
+          Credentials("mymavenrepo.com.write", "mymavenrepo.com", "myMavenRepo", password)
+        )
       ),
     resolvers ++= Seq(
       Resolver.sonatypeRepo("snapshots"),
